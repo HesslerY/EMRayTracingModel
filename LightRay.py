16463c806a3fbdfb3c89f4929800cb38b8e31228
Jean-LouisH <LouisLou512.e@gmail.com>
2020-01-14 12:09:38 -0500

    Added entities classes for the simulation

---
+++ LightRay.py
@@ -0,0 +1,4 @@
+class LightRay:
+
+    def __init__(self):
+        pass
---
+++ ObjectProperties.py
@@ -0,0 +1,11 @@
+class ObjectProperties:
+
+    def __init__(self):
+        self.propagationVelocity = 0.0
+        self.absorption = 0.0
+        self.reflectivity = 0.0
+        self.translucency = 0.0
+        self.dispersion = 0.0
+        self.polarization = 0.0
+        self.scattering = 0.0
+        self.fluorescence = 0.0
